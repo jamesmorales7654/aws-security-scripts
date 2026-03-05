1a389c0a12ab55384e1675b0b993cc3573a06e1c
James A. Morales <james.morales7654@gmail.com>
2026-03-05 17:50:41 -0500

    Create public_sg_check.py

---
+++ public_sg_check.py
@@ -0,0 +1,11 @@
+import boto3
+
+ec2 = boto3.client('ec2')
+
+groups = ec2.describe_security_groups()
+
+for g in groups['SecurityGroups']:
+    for perm in g['IpPermissions']:
+        for ip in perm.get('IpRanges', []):
+            if ip['CidrIp'] == '0.0.0.0/0':
+                print(f"Public SG detected: {g['GroupId']}")
